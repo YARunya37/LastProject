b5c64e94e6ba621cc7189b7685a045200091c969
YARunya37 <aroslavklucikov34199@gmail.com>
2026-06-23 14:49:13 +0500

    Some fixes in ForceBoostPlatform

--- Last Project/Assets/Scripts/ForceBoostPlatform.cs
+++ Last Project/Assets/Scripts/ForceBoostPlatform.cs
@@ -1,19 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class ForceBoostPlatform : MonoBehaviour
 {
 	[SerializeField] float boostForce;
 	[SerializeField] Vector2 direction;
-   	void OnTriggerEnter2D(Collider2D other)
-   	{
+	void OnTriggerEnter2D(Collider2D other)
+	{
 		if (other.gameObject.CompareTag("Player"))
 		{
 			Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
-			rb.velocity = Vector3.zero;
-			rb.AddForce(direction*boostForce, ForceMode2D.Impulse);
+			rb.velocity = Vector2.zero;
+			rb.AddForce(direction.normalized * boostForce, ForceMode2D.Impulse);
 		}
-   	}
+	}
 }
