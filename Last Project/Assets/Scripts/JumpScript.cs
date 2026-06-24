b7e18fea397ce1bdb2c2f1db266a002e90f29603
YARunya37 <aroslavklucikov34199@gmail.com>
2026-06-24 17:31:26 +0500

    Jump animation

--- Last Project/Assets/Scripts/JumpScript.cs
+++ Last Project/Assets/Scripts/JumpScript.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class JumpScript : PlayerController
 {
-	[SerializeField] AudioSource jump;	
+	[SerializeField] AudioSource jump;
 	[SerializeField] float jumpForce = 13f;
 	[SerializeField] float startScale = 3;
 	[SerializeField] float fallingScale = 2;
 	[SerializeField] LayerMask ground;
 	float fallingPoint = 0.2f;
+	bool isRotating = false;
+	PlayerController controller;
 	void Start()
 	{
 		rb = GetComponent<Rigidbody2D>();
+		controller = GetComponent<PlayerController>();
 	}
 	void Update()
 	{
@@ -21,10 +24,14 @@ public class JumpScript : PlayerController
 	void Jump()
 	{
 		if (Input.GetButtonDown("Jump") && IsGrounded())
-		{	
+		{
 			rb.gravityScale = startScale;
 			rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+
+			if (!isRotating)
+				StartCoroutine(RotatePlayer());
 		}
+
 		if (rb.velocity.y <= fallingPoint)
 		{
 			rb.gravityScale = fallingScale;
@@ -32,13 +39,34 @@ public class JumpScript : PlayerController
 	}
 	public bool IsGrounded()
 	{
-		if (Physics2D.Raycast(transform.position - new Vector3(0, 0.5f), Vector2.down, 0.1f, ground) 
-			|| Physics2D.Raycast(transform.position - new Vector3(0.5f, 0.5f), Vector2.down, 0.1f, ground) 
+		if (Physics2D.Raycast(transform.position - new Vector3(0, 0.5f), Vector2.down, 0.1f, ground)
+			|| Physics2D.Raycast(transform.position - new Vector3(0.5f, 0.5f), Vector2.down, 0.1f, ground)
 			|| Physics2D.Raycast(transform.position - new Vector3(-0.5f, 0.5f), Vector2.down, 0.1f, ground))
 		{
 			return true;
 		}
-		
+
 		return false;
+	}
+	IEnumerator RotatePlayer()
+	{
+		isRotating = true;
+
+		Quaternion startRot = transform.rotation;
+		Quaternion targetRot = Quaternion.Euler(0, 0, transform.eulerAngles.z + (-controller.FacingDirection * 90));
+
+		float duration = 0.25f;
+		float t = 0;
+
+		while (t < 1f)
+		{
+			t += Time.deltaTime / duration;
+			transform.rotation = Quaternion.Lerp(startRot, targetRot, t);
+			yield return null;
+		}
+
+		transform.rotation = Quaternion.Euler(0, 0, 0);
+
+		isRotating = false;
 	}
 }
