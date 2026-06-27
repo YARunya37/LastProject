b748600316ebac4fb7efd8796a72e03d3cd1a530
YARunya37 <aroslavklucikov34199@gmail.com>
2026-06-27 16:01:20 +0500

    BoostPlatform prefab: added animation

--- Last Project/Assets/Scripts/ForceBoostPlatform.cs
+++ Last Project/Assets/Scripts/ForceBoostPlatform.cs
@@ -4,10 +4,16 @@ public class ForceBoostPlatform : MonoBehaviour
 {
 	[SerializeField] float boostForce;
 	[SerializeField] Vector2 direction;
+	AbilityBoardPickup useAnimation;
+	void Start()
+	{
+		useAnimation = GetComponent<AbilityBoardPickup>();
+	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.gameObject.CompareTag("Player"))
 		{
+			useAnimation.PlayPickup();
 			Rigidbody2D rb = other.gameObject.GetComponent<Rigidbody2D>();
 			rb.velocity = Vector2.zero;
 			rb.AddForce(direction.normalized * boostForce, ForceMode2D.Impulse);
